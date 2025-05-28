a9daf69ba032ffebe7e5fc6a0651147173723714
Zeeeepa <zeeeepa@gmail.com>
2025-05-28 09:48:07 +0100

    dedupe.

--- src/codegen/extensions/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Extensions for the codegen package."""
-
-from graph_sitter.extensions.index.code_index import CodeIndex
-from graph_sitter.extensions.index.file_index import FileIndex
-
-__all__ = ["CodeIndex", "FileIndex"]
